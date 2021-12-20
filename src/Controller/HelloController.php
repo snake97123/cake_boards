551c165a1dcd1d339e3ff76069d87fcc66865e60
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-12-20 20:41:28 +0900

    Controllerの編集

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,13 +4,22 @@ namespace App\Controller;
 
 class HelloController extends AppController
 {
-  public $name = 'Hello';
-  public $autoRender = true;
-  public function index(){
-    $this->viewBuilder()->enableAutoLayout(false);
+  public function initialize()
+  {
+    $this->name = 'Hello';
+    $this->autoRender = false;
+    $this->viewBuilder()->autoLayout(true);
   }
 
-  public function other(){
-    echo "これはindex以外のページになります。";
+  public function index()
+  {
+    $this->viewBuilder()->autoLayout(true);
+    $this->render('/Hello/other');
+  }
+
+  public function other()
+  {
+    $this->viewBuilder()->autoLayout(false);
+    $this->autoRender = true;
   }
 }
