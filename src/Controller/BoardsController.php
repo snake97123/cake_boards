ecf22ff384df5f5b5b108f4172debefb1814efa0
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-01 17:03:58 +0900

    beforeSaveメソッド

--- src/Controller/BoardsController.php
+++ src/Controller/BoardsController.php
@@ -17,6 +17,7 @@ class BoardsController extends AppController {
     // $data = $this->Boards->find('list')->toArray();
    $data = $this->Boards->find('all');
    $this->set('data', $data);
+   $this->set('entity', $this->Boards->newEntity());
     // $this->set('data', $data->toArray());
     // $this->set('count', $data->count());
     // }
--- src/Model/Table/BoardsTable.php
+++ src/Model/Table/BoardsTable.php
@@ -4,9 +4,20 @@ namespace App\Model\Table;
 use Cake\ORM\Table;
 use Cake\Event\Event;
 use Cake\ORM\Query;
+use Cake\ORM\Entity;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\EventInterface;
 
 class BoardsTable extends Table {
   public function beforeFind(Event $event, Query $query){
     $query->order(['name' => 'ASC']);
+  }
+  public function beforeSave(Event $event, EntityInterface $entity){
+    $n = $this->find('all', ['conditions'=>['name'=>$entity->name]])->count();
+    if ($n == 0){
+      return true;
+    } else {
+      return false;
+    }
   }
 }
