6df85bed5db9398d36d57ffbfe89b02a88a7a6dd
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-05 20:43:05 +0900

    アソシエーション

---
+++ src/Controller/PeopleController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controller;
+
+class PeopleController extends AppController {
+  public function index($id = null){
+    $data = $this->People 
+       ->find('all')
+       ->contain(['Boards']);
+       $this->set('data', $data);
+  }
+}
---
+++ src/Model/Table/PeopleTable.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class PeopleTable extends Table {
+  public function initialize(array $config){
+    $this->hasMany('Boards');
+  }
+
+  public function validationDefault(Validator $validator){
+    $validator->integer('id');
+    $validator->notEmpty('name','必須項目です。');
+    $validator->notEmpty('password','必須項目です。');
+    $validator->notEmpty('comment', '必須項目です。');
+
+    return $validator;
+  }
+
+  public function buildRules(RulesChecker $rules){
+    $rules->isUnique(['name'], 'すでに登録済みです。');
+    return $rules;
+  }
+}
+
