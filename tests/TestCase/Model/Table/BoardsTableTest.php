db2d2b1977309bd9fb6249d5ecc47734aa7ac6c8
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-16 11:00:04 +0900

    TableTestの実装

--- tests/TestCase/Model/Table/BoardsTableTest.php
+++ tests/TestCase/Model/Table/BoardsTableTest.php
@@ -80,4 +80,13 @@ class BoardsTableTest extends TestCase
     {
         $this->markTestIncomplete('Not implemented yet.');
     }
+
+    /** find Board test */
+    public function testBoardsTableFind() {
+        $result = $this->BoardsTable->find('all')->first();
+        $this->assertFalse(empty($result));
+        $this->assertTrue(is_a($result, 'App\Model\Entity\Board'));
+        $this->assertEquals($result->id, 1001);
+        $this->assertStringStartsWith('test title 1', $result->title);
+    }
 }
