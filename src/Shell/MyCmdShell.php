8ea2bd0a288509249670c9978d77a1196364607b
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-25 22:39:59 +0900

    modelの利用

--- src/Shell/MyCmdShell.php
+++ src/Shell/MyCmdShell.php
@@ -4,13 +4,23 @@ namespace App\Shell;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use Cake\Log\Log;
+use Cake\ORM\TableRegistry;
 
 class MyCmdShell extends Shell {
-  public function main($n){
-    $res = 0;
-    for($i = 1; $i <= $n; $i++) {
-      $res += $i;
-    }
-    $this->out("1から{$n}までの合計:" . $res);
+  public function initialize(){
+    parent::initialize();
+    $this->loadModel('Boards');
   }
+
+  public function main($num){
+    $data = $this->Boards->get($num);
+    $this->out(print_r($data->toArray()));
+  }
+  // public function main(...$num){
+  //   $res = 0;
+  //   foreach($num as $n){
+  //     $res += $n;
+  //   }
+  //   $this->out("合計: " . $res);
+  // }
 }
