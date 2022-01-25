6058a54ffa5e16d1240bda7af1472af6a74efd65
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-25 22:28:53 +0900

    引数渡しへの対応

--- src/Shell/MyCmdShell.php
+++ src/Shell/MyCmdShell.php
@@ -6,7 +6,11 @@ use Cake\Console\Shell;
 use Cake\Log\Log;
 
 class MyCmdShell extends Shell {
-  public function main(){
-    $this->out("this is MyCmd Shell.");
+  public function main($n){
+    $res = 0;
+    for($i = 1; $i <= $n; $i++) {
+      $res += $i;
+    }
+    $this->out("1から{$n}までの合計:" . $res);
   }
 }
