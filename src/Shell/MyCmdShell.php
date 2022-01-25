88bc686dea0ab9bea87c67f0ca026261e9f3cae8
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-26 04:02:56 +0900

    メソッドの利用

--- src/Shell/MyCmdShell.php
+++ src/Shell/MyCmdShell.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Shell;
 
+use Cake\Console\ConsoleOutput;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use Cake\Log\Log;
@@ -19,28 +20,44 @@ class MyCmdShell extends Shell {
     $t = $this->in('テーブルを選択:', ['B', 'P'], 'B');
     $t = strtoupper($t);
     $table = null;
-    $id = $this->in('ID番号を入力:', null, 1);
+    $n = $this->in('ID番号を入力:', null, 1);
     $data = null;
     switch($t){
       case 'B':
-        $table = 'Boards';
-        $this->loadModel('Boards');
-        $data = $this->Boards->get($id);
+        $this->boards($n);
+        // $table = 'Boards';
+        // $this->loadModel('Boards');
+        // $data = $this->Boards->get($id);
         break;
       case 'P':
-        $table = 'People';
-        $this->loadModel('People');
-        $data = $this->People->get($id);
+        $this->people($n);
+        // $table = 'People';
+        // $this->loadModel('People');
+        // $data = $this->People->get($id);
         break;
       default:
         $this->info("can't access Database...");
         exit();
     }
-    // $data = $this->Boards->get($num);
-    $this->out();
-    $this->out("※{$table} id={$id} のレコード:");
-    $this->out(print_r($data->toArray()));
   }
+    public function boards($id){
+      $this->loadModel('Boards');
+      $data = $this->Boards->get($id);
+      $this->out("※Boards id={$id}");
+      $this->out(print_r($data->toArray()));
+    }
+    public function people($id){
+      $this->loadModel('People');
+      $data = $this->People->get($id);
+      $this->out("※People id={$id}");
+      $this->out(print_r($data->toArray()));
+    }
+
+    // $data = $this->Boards->get($num);
+    // $this->out();
+    // $this->out("※{$table} id={$id} のレコード:");
+    // $this->out(print_r($data->toArray()));
+  
   // public function main(...$num){
   //   $res = 0;
   //   foreach($num as $n){
