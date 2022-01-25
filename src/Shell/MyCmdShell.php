33e7b82e1e8e80e7dd651498cbef6d2ebcc33836
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-26 03:49:15 +0900

    ユーザーからの入力

--- src/Shell/MyCmdShell.php
+++ src/Shell/MyCmdShell.php
@@ -9,11 +9,36 @@ use Cake\ORM\TableRegistry;
 class MyCmdShell extends Shell {
   public function initialize(){
     parent::initialize();
-    $this->loadModel('Boards');
+    // $this->loadModel('Boards');
   }
 
-  public function main($num){
-    $data = $this->Boards->get($num);
+  public function main(){
+    $this->out('※以下のテーブルが利用できます。');
+    $this->out('[B]oards');
+    $this->out('[P]eople');
+    $t = $this->in('テーブルを選択:', ['B', 'P'], 'B');
+    $t = strtoupper($t);
+    $table = null;
+    $id = $this->in('ID番号を入力:', null, 1);
+    $data = null;
+    switch($t){
+      case 'B':
+        $table = 'Boards';
+        $this->loadModel('Boards');
+        $data = $this->Boards->get($id);
+        break;
+      case 'P':
+        $table = 'People';
+        $this->loadModel('People');
+        $data = $this->People->get($id);
+        break;
+      default:
+        $this->info("can't access Database...");
+        exit();
+    }
+    // $data = $this->Boards->get($num);
+    $this->out();
+    $this->out("※{$table} id={$id} のレコード:");
     $this->out(print_r($data->toArray()));
   }
   // public function main(...$num){
