978d048bf38887d29dd29b2d4310e260bcc98aa4
snake97123 <73522198+snake97123@users.noreply.github.com>
2022-01-27 06:44:08 +0900

    オプション設定

--- src/Shell/MyCmdShell.php
+++ src/Shell/MyCmdShell.php
@@ -9,7 +9,7 @@ use Cake\ORM\TableRegistry;
 
 class MyCmdShell extends Shell {
   public $tasks = ['Db'];
-  public function main(){
+  public function main($opt = null){
     $this->out("※以下のテーブルが利用できます。");
     $this->out('[B]oards');
     $this->out('[P]eople');
@@ -29,8 +29,71 @@ class MyCmdShell extends Shell {
         $this->info("can't access Database...");
         exit();
     }
-    $this->Db->main();
+    // $this->Db->main();
     $this->Db->get($table, $id);
+  }
+
+  public function boards($id = null){
+    if ($id == null){
+      $n = $this->in('ID番号を入力:', null, 1);
+    } else {
+      $n = $id;
+    }
+    $this->table('boards', $id);
+  }
+
+  public function people($id = null){
+    if ($id == null){
+      $n = $this->in('ID番号を入力:', null, 1);
+    } else {
+      $n = $id;
+    }
+    $this->table('people', $id);
+  }
+
+  public function table($table, $id){
+    if ($this->params['db']){
+      $this->Db->main();
+      $this->Db->get($table, $id);
+    } else {
+      $data = TableRegistry::get($table)->get($id);
+      $this->out(print_r($data->toArray()));
+    }
+  }
+  
+  public function getOptionParser() {
+    $parser = new ConsoleOptionParser('mycmd');
+    $parser->description(
+      "これは、サンプルで作成したシェルプログラムです。"
+    )->addArgument(
+      'boards $n',
+      [
+        'help' => 'Boardsテーブルの利用。ID番号をつけて実行。',
+        'require' => false
+      ]
+    )->addArgument(
+      'people $n',
+      [
+        'help' => 'Peopleテーブルの利用。ID番号をつけて実行。',
+        'require' => false
+      ]
+    )->addArgument(
+      'table $t $n',
+      [
+        'help' => 'テーブル名とID番号をつけて実行。',
+        'require' => false
+      ]
+    )->addOption(
+      'db',
+      [
+        'short' => 'g',
+        'boolean' => true,
+        'help' => 'DbTaskを使います。'
+      ]
+    )->epilog(
+      '※便利に使ってね！'
+    );
+    return $parser;
   }
 
   public function bh ($target) {
